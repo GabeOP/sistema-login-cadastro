ef8de4c25b6d30de927be06878f245d4dd1a713c
GabeOP <gabrieloliveirap1@hotmail.com>
2023-04-04 22:16:50 -0300

    feat: rota privada

--- src/server.js
+++ src/server.js
@@ -5,13 +5,15 @@ const app = express();
 const cors = require("cors");
 app.use(cors());
 
-const bodyParser = require("body-parser")
+const bodyParser = require("body-parser");
+const auth = require("./middleware/auth.jwt");
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
 app.get("/", Controller.buscar)
 app.post("/cadastrar", Controller.cadastrar)
 app.post("/entrar", Controller.entrar)
+app.get("/rota-privada", auth.jsonwebtoken, (req, res)=>{console.log("Teste")})
 app.delete("/:id", Controller.excluir)
 
 const PORT = 3000
