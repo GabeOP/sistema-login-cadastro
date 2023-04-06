da80e38fc19f3bac0958c6a99082576760386efc
GabeOP <gabrieloliveirap1@hotmail.com>
2023-04-06 17:10:58 -0300

    feat: autenticacao rota /

--- src/server.js
+++ src/server.js
@@ -10,10 +10,9 @@ const auth = require("./middleware/auth.jwt");
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
-app.get("/", Controller.buscar)
+app.get("/", auth.jsonwebtoken, Controller.buscar)
 app.post("/cadastrar", Controller.cadastrar)
 app.post("/entrar", Controller.entrar)
-app.get("/rota-privada", auth.jsonwebtoken, (req, res)=>{console.log("Teste")})
 app.delete("/:id", Controller.excluir)
 
 const PORT = 3000
